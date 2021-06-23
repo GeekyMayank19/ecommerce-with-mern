c4578889abc6d441903a302d183434077bc0c786
Mayank Sharma <46962063+GeekyMayank19@users.noreply.github.com>
2021-06-23 11:03:46 +0530

    new projcet of mern

---
+++ frontend/build/precache-manifest.74875e96229570fd3b131acd2483c776.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c4484acc19085c0c90c6b77d9ea49a8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f90b7f15c75663ffaf6e",
+    "url": "/static/css/main.00cf29bb.chunk.css"
+  },
+  {
+    "revision": "8d42b92a34055e108536",
+    "url": "/static/js/2.61997b1b.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.61997b1b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f90b7f15c75663ffaf6e",
+    "url": "/static/js/main.d0015761.chunk.js"
+  },
+  {
+    "revision": "2b55614ca07f93359093",
+    "url": "/static/js/runtime-main.bfd8333c.js"
+  }
+]);
---
+++ frontend/src/backend.js
@@ -0,0 +1,1 @@
+export const API = "https://ecommerce-backend-2.herokuapp.com/api";
